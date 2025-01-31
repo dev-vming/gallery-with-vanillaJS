239c10f6c11a00515c085bba2aa57a81d44ad4bf
dev-vming <dev.vming@gmail.com>
2025-01-31 23:24:49 +0900

    Feat: 이미지 모서리 둥글게 처리

--- gallery.js
+++ gallery.js
@@ -6,7 +6,7 @@ export default function gallery(imageSrcList, width, height, row, column) {
   canvas.height = height;
 
   const ctx = canvas.getContext('2d');
-  ctx.fillStyle = '#000';
+  ctx.fillStyle = '#fff';
   ctx.fillRect(0, 0, width, height);
 
   const itemWidth = canvas.width / column;
@@ -22,6 +22,17 @@ export default function gallery(imageSrcList, width, height, row, column) {
     return image;
   });
 
+  function drawClipPath(left, top, width, height, radius) {
+    ctx.beginPath();
+    ctx.moveTo(left + radius, top);
+    ctx.arcTo(left + width, top, left + width, top + height, radius);
+    ctx.arcTo(left + width, top + height, left, top + height, radius);
+    ctx.arcTo(left, top + height, left, top, radius);
+    ctx.arcTo(left, top, left + width, top, radius);
+    ctx.closePath();
+    ctx.clip();
+  }
+
   function drawItem(item, idx) {
     const left = (idx % column) * itemWidth;
     const top = Math.trunc(idx / column) * itemHeight;
@@ -29,7 +40,11 @@ export default function gallery(imageSrcList, width, height, row, column) {
     const destTop = top + itemMargin;
     const destWidth = itemWidth - itemMargin * 2;
     const destHeight = itemHeight - itemMargin * 2;
+
+    ctx.save();
+    drawClipPath(destLeft, destTop, destWidth, destHeight, 10);
     ctx.drawImage(item,destLeft,destTop,destWidth,destHeight);
+    ctx.restore();
   }
 
   return canvas;
